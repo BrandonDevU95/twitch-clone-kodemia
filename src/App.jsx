1075e06a2dadb69f5e728a6c69b0f1c6f1d1333a
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-15 10:36:58 -0600

    feat: Add Chanels component for displaying Twitch channels

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Chanels from './components/Chanels';
 import Navbar from './components/Navbar';
 
 export default function App() {
 	return (
-		<>
+		<div className="grid grid-rows-[auto_1fr] grid-cols-1 min-h-screen">
 			<Navbar />
-		</>
+			<div className="grid grid-cols-[240px_1fr]">
+				<Chanels />
+			</div>
+		</div>
 	);
 }
