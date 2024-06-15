17bdbf487057c032ade94c5f489b2310581a6f82
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-15 10:55:43 -0600

    feat: Add lazy loading for images in Browse component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Browse from './components/Browse';
 import Chanels from './components/Chanels';
 import Navbar from './components/Navbar';
 
@@ -7,6 +8,21 @@ export default function App() {
 			<Navbar />
 			<div className="grid grid-cols-[240px_1fr]">
 				<Chanels />
+				<div className="p-4 flex flex-col gap-7">
+					<Browse />
+					<div className="flex text-sm font-semibold gap-3">
+						<a
+							className="font-semibold text-xl p-1 text-purple-400 border-b-purple-400 border-b-2 pb-3"
+							href="#">
+							Categorías
+						</a>
+						<a
+							className="font-semibold text-xl p-1 hover:text-purple-400"
+							href="#">
+							Canales en directo
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
