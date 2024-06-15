113a7681115c80eac4c41438b4635e10239937af
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-15 10:55:13 -0600

    feat: Add categories data to the project

---
+++ src/lib/categories.js
@@ -0,0 +1,24 @@
+const categories = [
+	{
+		title: 'Juegos',
+		img: 'https://static-cdn.jtvnw.net/c3-vg/verticals/gaming.svg',
+	},
+	{
+		title: 'IRL',
+		img: 'https://static-cdn.jtvnw.net/c3-vg/verticals/irl.svg',
+	},
+	{
+		title: 'Música',
+		img: 'https://static-cdn.jtvnw.net/c3-vg/verticals/music.svg',
+	},
+	{
+		title: 'Creative',
+		img: 'https://static-cdn.jtvnw.net/c3-vg/verticals/creative.svg',
+	},
+	{
+		title: 'Esports',
+		img: 'https://static-cdn.jtvnw.net/c3-vg/verticals/esports.svg',
+	},
+];
+
+export default categories;
