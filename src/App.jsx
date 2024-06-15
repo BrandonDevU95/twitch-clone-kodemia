1f1923a0ca40d3597a9434347d63f9645ab4dad1
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-15 11:35:21 -0600

    refactor: Update App component grid layout for responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Navbar from './components/Navbar';
 
 export default function App() {
 	return (
-		<div className="grid grid-rows-[auto_1fr] grid-cols-1 min-h-screen">
+		<div className="w-full grid grid-rows-[50px_1fr] min-h-screen">
 			<Navbar />
-			<div className="grid grid-cols-[240px_1fr]">
+			<div className="grid lg:grid-cols-[240px_1fr] sm:grid-cols-[50px_1fr]">
 				<Chanels />
 				<div className="p-4 flex flex-col gap-7">
 					<Browse />
